ccb3baaabbfa975d14051497e1499f3cc49a284d
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2024-01-11 01:56:24 +0500

    updated routing

--- admin-panel/src/app/app-routing.module.ts
+++ admin-panel/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import path from 'path';
+import { MainComponent } from './pages/main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'main', component: MainComponent },
+  { path: '', redirectTo:'main', pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
