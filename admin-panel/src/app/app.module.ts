840191f5e46fe36aba8ae8bd88945cf5cec1d9e3
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2024-01-10 16:06:35 +0500

    created main component

--- admin-panel/src/app/app.module.ts
+++ admin-panel/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
