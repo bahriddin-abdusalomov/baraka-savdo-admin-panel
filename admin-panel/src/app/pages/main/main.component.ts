6fd5a2e26bbda49c4ad3388849d8e4ee4ead2d7b
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2024-01-11 18:22:24 +0500

    udpated main component

--- admin-panel/src/app/pages/main/main.component.ts
+++ admin-panel/src/app/pages/main/main.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrl: './main.component.scss'
 })
 export class MainComponent {
-
+  images: string[] = [
+    '../../../assets/images/logo/apple.svg',
+    '../../../assets/images/logo/BQ_Logo.svg.svg',
+    '../../../assets/images/logo/Logo_NIKE.svg.svg'
+  ];
 }
