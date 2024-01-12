37173f96d86677f5aa571550dcb95997c2b07461
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2024-01-12 16:48:31 +0500

    udpated

--- admin-panel/src/app/app-routing.module.ts
+++ admin-panel/src/app/app-routing.module.ts
@@ -2,14 +2,20 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import path from 'path';
 import { MainComponent } from './pages/main/main.component';
+import { LoginComponent } from './pages/auth-module/login/login.component';
+import { RegisterComponent } from './pages/auth-module/register/register.component';
+import { ResetPasswordComponent } from './pages/auth-module/reset-password/reset-password.component';
 
 const routes: Routes = [
   { path: 'main', component: MainComponent },
-  { path: '', redirectTo:'main', pathMatch:'full'}
+  { path: 'login', component: LoginComponent },
+  { path: 'reset', component:ResetPasswordComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '', redirectTo: 'main', pathMatch: 'full' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
