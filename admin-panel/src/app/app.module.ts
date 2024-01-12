6f3f589f991ebcb335c9cadffe91292dc2b5c175
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2024-01-12 05:06:06 +0500

    updated app module

--- admin-panel/src/app/app.module.ts
+++ admin-panel/src/app/app.module.ts
@@ -6,12 +6,22 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainComponent } from './pages/main/main.component';
 import { CarouselComponent } from './components/carousel/carousel.component';
+import { FaqComponent } from './components/faq/faq.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
     CarouselComponent,
+    FaqComponent,
+    FooterComponent,
+    NavbarComponent,
+    RegisterComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
