03f4bcc4f64e629adb79cf8212acad3da5de4f9f
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2024-01-17 21:49:30 +0500

    commit

--- admin-panel/src/app/pages/auth-module/register/register.component.ts
+++ admin-panel/src/app/pages/auth-module/register/register.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+// register.component.ts
+
+import { Component, INJECTOR, Inject, OnInit, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../../services/auth/auth.service';
+
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {
+export class RegisterComponent{
+  registrationForm: FormGroup;
 
+  constructor(private formBuilder: FormBuilder,
+    private service: AuthService) {}
+
+  ngOnInit(): void {
+    this.registrationForm = this.formBuilder.group({
+      first_name: ['', Validators.required],
+      last_name: ['', Validators.required],
+      phone_number: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  registerUser(): void {
+    if (this.registrationForm.valid) {
+      // O'z logikangizni yozishingiz mumkin
+       alert('Foydalanuvchi ro\'yxatdan o\'tdi:' + this.registrationForm.getRawValue());
+    } else {
+      // Validatsiyadan o'tmagan maydonlar bor
+       alert('Forma noto\'g\'ri to\'ldirilgan' + this.registrationForm.getRawValue());
+    }
+  }
 }
