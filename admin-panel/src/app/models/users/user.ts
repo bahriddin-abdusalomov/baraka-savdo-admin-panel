86528074a52ba56141347c412d51ef0458f52a68
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2024-01-17 21:49:18 +0500

    commit

---
+++ admin-panel/src/app/models/users/user.ts
@@ -0,0 +1,14 @@
+export interface User {
+    id: number;
+    firstName: string;
+    lastName: string;
+    phone_number: string;
+    password: string;
+    passport_seria_number: string;
+    is_male: boolean;
+    birth_date: Date;
+    country: string;
+    region: string;
+    image_path: string;
+  }
+  
