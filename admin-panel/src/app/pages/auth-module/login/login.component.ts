fd1cd22bf6791eccc1f6bdbc7dd407e30db9a243
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2024-01-19 03:01:32 +0500

    commit

--- admin-panel/src/app/pages/auth-module/login/login.component.ts
+++ admin-panel/src/app/pages/auth-module/login/login.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../../services/auth/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +9,33 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  loginForm: FormGroup;
 
+  constructor(private formBuilder: FormBuilder,
+    private service: AuthService){ }
+
+  ngOnInit(): void{
+    this.loginForm = this.formBuilder.group({
+      PhoneNumber: ['', Validators.required],
+      Password: ['', Validators.required]
+    });
+  }
+
+  loginUser(): void{
+    if(this.loginForm.valid){
+      let data = this.service.login(this.loginForm.getRawValue())
+      .subscribe({
+        next:() => {
+          alert("Kirish paroli togri!!!")
+        },
+        error: () => {
+          alert("Kalla qoyding uka !")
+          console.log(data);
+        }
+      })
+    }
+    else{
+      alert("Parol notogri kiritildi!!")
+    }
+  }
 }
