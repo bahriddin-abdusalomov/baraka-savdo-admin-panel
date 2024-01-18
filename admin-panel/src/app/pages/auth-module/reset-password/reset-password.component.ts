42589020ec89516fe324aef342f643b41b1ce832
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2024-01-19 03:02:00 +0500

    commit

--- admin-panel/src/app/pages/auth-module/reset-password/reset-password.component.ts
+++ admin-panel/src/app/pages/auth-module/reset-password/reset-password.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../../services/auth/auth.service';
 
 @Component({
   selector: 'app-reset-password',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrl: './reset-password.component.scss'
 })
 export class ResetPasswordComponent {
+  resetPasswordForm: FormGroup;
 
+  constructor(private formBuilder: FormBuilder,
+    private service: AuthService){}
+
+  ngOnInit(): void{
+    this.resetPasswordForm = this.formBuilder.group({
+      PhoneNumber: ['', Validators.required],
+      Password: ['', Validators.required],
+    });
+  }
+  resetPasswordUser(): void{
+    if(this.resetPasswordForm.valid){
+      this.service.resetPassword(this.resetPasswordForm.getRawValue())
+      .subscribe({
+        next: () => {
+          alert("Parol yangilandi")
+        },
+        error: () => {
+          alert("Ishimiz oxshamayapri")
+        }
+      })
+    }
+  }
 }
