b9d0f711aba742e0ad9066d2be31543847c2c2f0
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2024-01-21 02:56:48 +0500

    commit

--- admin-panel/src/app/app-routing.module.ts
+++ admin-panel/src/app/app-routing.module.ts
@@ -5,12 +5,16 @@ import { MainComponent } from './pages/main/main.component';
 import { LoginComponent } from './pages/auth-module/login/login.component';
 import { RegisterComponent } from './pages/auth-module/register/register.component';
 import { ResetPasswordComponent } from './pages/auth-module/reset-password/reset-password.component';
+import { CommonComponent } from './pages/common/common.component';
+import { ProductComponent } from './pages/product-module/product/product.component';
 
 const routes: Routes = [
   { path: 'main', component: MainComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'reset', component:ResetPasswordComponent },
+  { path: 'reset', component: ResetPasswordComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'seller', component: CommonComponent },
+  { path: 'seller/product/all', component: ProductComponent },
   { path: '', redirectTo: 'main', pathMatch: 'full' },
 ];
 
