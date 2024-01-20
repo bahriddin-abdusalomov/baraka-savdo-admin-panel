abde9b4d97b536df4418cf58c0243b5e2c19e5bb
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2024-01-21 02:58:04 +0500

    commit

--- admin-panel/src/app/components/navbar/navbar.component.spec.ts
+++ admin-panel/src/app/components/navbar/navbar.component.spec.ts
@@ -8,10 +8,9 @@ describe('NavbarComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [NavbarComponent]
-    })
-    .compileComponents();
-    
+      declarations: [NavbarComponent],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(NavbarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
