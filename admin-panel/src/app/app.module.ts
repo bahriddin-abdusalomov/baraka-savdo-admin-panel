cca71ff3912c555e5ad8d1d2e145b8983de8563b
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2024-01-21 02:57:17 +0500

    commit

--- admin-panel/src/app/app.module.ts
+++ admin-panel/src/app/app.module.ts
@@ -17,6 +17,23 @@ import { LoginComponent } from './pages/auth-module/login/login.component';
 import { ResetPasswordComponent } from './pages/auth-module/reset-password/reset-password.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
+import { AddProductComponent } from './pages/product-module/add-product/add-product.component';
+import { ProductPricingComponent } from './pages/product-module/product-pricing/product-pricing.component';
+import { ProductPropertiesComponent } from './pages/product-module/product-properties/product-properties.component';
+import { CommonComponent } from './pages/common/common.component';
+import { VerticalNavComponent } from './components/vertical-nav/vertical-nav.component';
+import { ProductComponent } from './pages/product-module/product/product.component';
+import { ProductAllNavComponent } from './components/product-all-nav/product-all-nav.component';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { DialogComponent } from './components/dialog/dialog.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatIconModule } from '@angular/material/icon';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
 
 @NgModule({
   declarations: [
@@ -29,8 +46,33 @@ import { ReactiveFormsModule } from '@angular/forms';
     RegisterComponent,
     LoginComponent,
     ResetPasswordComponent,
+    AddProductComponent,
+    ProductPricingComponent,
+    ProductPropertiesComponent,
+    CommonComponent,
+    VerticalNavComponent,
+    ProductComponent,
+    ProductAllNavComponent,
+    DialogComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, HttpClientModule, ReactiveFormsModule],
+  imports: [
+    NgxPaginationModule,
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatIconModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
+  ],
   providers: [provideClientHydration()],
   bootstrap: [AppComponent],
 })
