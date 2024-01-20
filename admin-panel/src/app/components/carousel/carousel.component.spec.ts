34ce12ca0939ab465399bdc6c1cef9eadc95dc43
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2024-01-21 02:57:07 +0500

    commit

--- admin-panel/src/app/components/carousel/carousel.component.spec.ts
+++ admin-panel/src/app/components/carousel/carousel.component.spec.ts
@@ -7,9 +7,8 @@ describe('CarouselComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CarouselComponent ]
-    })
-    .compileComponents();
+      declarations: [CarouselComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
