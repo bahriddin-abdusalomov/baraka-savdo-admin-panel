c07046de3755077bddef1733bddcd6aeae799aa6
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2024-01-22 18:12:33 +0500

    commit

--- admin-panel/src/app/services/products/product.service.ts
+++ admin-panel/src/app/services/products/product.service.ts
@@ -5,27 +5,38 @@ import { map } from 'rxjs';
 // import { ProductDto } from '../../dtos/products/productDto';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-  addProduct(product: any){
-    return this.http.post<any>("hhttps://localhost:7030/api/Products/Create", product)
-  }
-
-  getAllProduct(){
-    return this.http.get<any>("https://localhost:7030/api/Products/GetAll?page=1").pipe(
-      map(products => products.sort((a:Product, b:Product) => a.id - b.id))
+  addProduct(product: any) {
+    return this.http.post<any>(
+      'hhttps://localhost:7030/api/Products/Create',
+      product
     );
   }
 
-  updateProduct(id: number, product: any){
-    return this.http.put<Product>("https://localhost:7030/api/Products/Update?id=" + id, product)
+  getAllProduct() {
+    return this.http
+      .get<any>('https://localhost:7030/api/Products/GetAll?page=1')
+      .pipe(
+        map((products) =>
+          products.sort((a: Product, b: Product) => a.id - b.id)
+        )
+      );
   }
 
-  deleteProduct(id: number){
-    return this.http.delete<any>("https://localhost:7030/api/Products/Delete?categoryId=" + id)
+  updateProduct(id: number, product: any) {
+    return this.http.put<Product>(
+      'https://localhost:7030/api/Products/Update?id=' + id,
+      product
+    );
+  }
+
+  deleteProduct(id: number) {
+    return this.http.delete<any>(
+      'https://localhost:7030/api/Products/Delete?categoryId=' + id
+    );
   }
 }
