169b43ef614cdd77a9ff13227d505edf5985feff
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2024-01-25 14:29:22 +0500

    commit

--- admin-panel/src/app/services/auth/auth.service.ts
+++ admin-panel/src/app/services/auth/auth.service.ts
@@ -7,19 +7,19 @@ import { HttpClient } from '@angular/common/http';
 
 export class AuthService {
 
-  apiUrl: string = "https://localhost:7030/api/Authorization/";
+  apiUrl: string = "https://185.217.131.187:7030/api/Authorization/";
 
   constructor(private http: HttpClient) { }
 
   register(register: any){
-    return this.http.post<any>("https://localhost:7030/api/Authorization/Register", register)
+    return this.http.post<any>("https://185.217.131.187:7030/api/Authorization/Register", register)
   }
 
   login(login: any){
-    return this.http.get<any>("https://localhost:7030/api/Authorization/Login", login)
+    return this.http.get<any>("https://185.217.131.187:7030/api/Authorization/Login", login)
   }
 
   resetPassword(resetPassword: any){
-    return this.http.put<any>("https://localhost:7030/api/Authorization/ResetPassword", resetPassword)
+    return this.http.put<any>("https://185.217.131.187:7030/api/Authorization/ResetPassword", resetPassword)
   }
 }
