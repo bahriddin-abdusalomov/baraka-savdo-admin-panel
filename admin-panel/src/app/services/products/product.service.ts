fb94c90d4bd814f21f643d5e64d5aff5f30b3a46
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2024-01-25 14:29:28 +0500

    commit

--- admin-panel/src/app/services/products/product.service.ts
+++ admin-panel/src/app/services/products/product.service.ts
@@ -12,14 +12,14 @@ export class ProductService {
 
   addProduct(product: any) {
     return this.http.post<any>(
-      'hhttps://localhost:7030/api/Products/Create',
+      'hhttps://185.217.131.187:7030/api/Products/Create',
       product
     );
   }
 
   getAllProduct() {
     return this.http
-      .get<any>('https://localhost:7030/api/Products/GetAll?page=1')
+      .get<any>('https://185.217.131.187:7030/api/Products/GetAll?page=1')
       .pipe(
         map((products) =>
           products.sort((a: Product, b: Product) => a.id - b.id)
@@ -29,14 +29,14 @@ export class ProductService {
 
   updateProduct(id: number, product: any) {
     return this.http.put<Product>(
-      'https://localhost:7030/api/Products/Update?id=' + id,
+      'https://185.217.131.187:7030/api/Products/Update?id=' + id,
       product
     );
   }
 
   deleteProduct(id: number) {
     return this.http.delete<any>(
-      'https://localhost:7030/api/Products/Delete?categoryId=' + id
+      'https://185.217.131.187:7030/api/Products/Delete?categoryId=' + id
     );
   }
 }
