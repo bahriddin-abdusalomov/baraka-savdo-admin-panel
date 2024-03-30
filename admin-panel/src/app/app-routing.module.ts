3fb01c3c90cb11a160bc76453ad9e821d1d880ae
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2024-03-30 21:39:31 +0500

    updated

--- admin-panel/src/app/app-routing.module.ts
+++ admin-panel/src/app/app-routing.module.ts
@@ -7,11 +7,15 @@ import { RegisterComponent } from './pages/auth-module/register/register.compone
 import { ResetPasswordComponent } from './pages/auth-module/reset-password/reset-password.component';
 import { CommonComponent } from './pages/common/common.component';
 import { ProductComponent } from './pages/product-module/product/product.component';
+import { ConfirmationCodeComponent } from './pages/auth-module/confirmation-code/confirmation-code.component';
+import { EmailVerificationComponent } from './pages/auth-module/email-verification/email-verification.component';
 
 const routes: Routes = [
   { path: 'main', component: MainComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'verification', component: EmailVerificationComponent},
   { path: 'reset', component: ResetPasswordComponent },
+  { path: 'confirmation', component: ConfirmationCodeComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'seller', component: CommonComponent },
   { path: 'seller/product/all', component: ProductComponent },
